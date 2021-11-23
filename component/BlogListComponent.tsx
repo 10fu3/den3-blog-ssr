04d4711170324cb9d038eae81701c20d4718fd1f
10fu3 <msf.1192@gmail.com>
2021-11-23 16:49:24 +0900

    complete BlogListComponent.tsx

--- component/BlogListComponent.tsx
+++ component/BlogListComponent.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {Grid} from "@mui/material";
 import HoverBlogItem from "./HoverBlogItem";
+import {BlogListEntity} from "../model/BlogListPage";
 
-const BlogListComponent:React.FC<{}> = (props)=>{
+const BlogListComponent:React.FC<{e:BlogListEntity[]}> = (props)=>{
     return <div>
         <h1 style={{marginTop:30,marginBottom:30,color:"#2a2c2e"}}>ブログ一覧</h1>
         <Grid container>
             {
-                d.e.map((e,i)=>{
+                props.e.map((e,i)=>{
                     return <Grid item key={e.title} style={{padding:2}} xs={12} sm={6} md={4}>
                         <HoverBlogItem e={e} height={320}/>
                     </Grid>
@@ -16,3 +17,5 @@ const BlogListComponent:React.FC<{}> = (props)=>{
         </Grid>
     </div>
 }
+
+export default BlogListComponent
